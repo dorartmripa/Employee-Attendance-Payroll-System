ec5fb53fc54770ff2ba767a31ad2a03ee65b00da
Salih Mripa <smripa@hotmail.com>
2025-12-15 14:24:40 +0000

    Update Code

--- Employee Attendance & Payroll System.py
+++ Employee Attendance & Payroll System.py
@@ -40,13 +40,12 @@ def add_employee():
 
 def update_employee():
     while True:
-        while True:
-            try:
-                print("To update a employee, please add the following: ")
-                id = int(input("Enter the id of the employee you would like to update:"))
-                break
-            except ValueError:
-                print("Uh-oh! Please enter a number!")
+        try:
+            print("To update a employee, please add the following: ")
+            id = int(input("Enter the id of the employee you would like to update:"))
+        except ValueError:
+            print("Uh-oh! Please enter a number!")
+            continue
 
         cursor.execute("SELECT 1 from employees WHERE id = ? LIMIT 1", (id,))
         exists = cursor.fetchone()
@@ -56,18 +55,101 @@ def update_employee():
             continue
 
         while True:
-            choice = input("What woudl you like to update:\n- Name\n- Position\n- Hourly Rate").strip().lower()
+            choice = input("""What would you like to update:
+                            \n- Name
+                            \n- Position
+                            \n- Hourly Rate""").strip().lower()
 
             if choice not in ["name", "position", "hourly_rate"]:
                 print("Uh-oh! Please enter a valid option!")
                 continue
             
-            update = input(f"Enter the new information for {choice}: ")
+            while True:
+                update = input(f"Enter the new information for {choice}: ")
 
-            if choice == "name":
-                cursor.execute("UPDATE employees SET name = ? WHERE id = ?", (update.strip().lower().capitalize(), id))
-            elif choice == "position":
-                cursor.execute("UPDATE employees SET position = ? WHERE id = ?", (updat))
+                if update == "hourly_rate":
+                    try:
+                        update = float(update)
+                        cursor.execute("UPDATE employees SET hourly_rate = ? WHERE id = ?", (update, id))
+                    except ValueError:
+                        print("Uh-oh! Please enter a number!")
+                        continue
+                else:
+                    cursor.execute(f"UPDATE employees SET {choice} = ? WHERE id = ?", (update.strip().lower().capitalize(), id))
+                
+                database.commit()
+                print("The record was updated successfully!")
+                break
+            break
+        break
+
+def remove_employee():
+    while True:
+        try:
+            print("To remove an employee, please add the following: ")
+            id = int(input("Enter the id of the employee you would like to remove: "))
+        except ValueError:
+            print("Uh-oh! Please enter a number!")
+            continue
+
+        cursor.execute("SELECT 1 from employees WHERE id = ? LIMIT 1", (id,))
+        exists = cursor.fetchone()
+
+        if not exists:
+            print("Uh-oh! There is no employee with that id!") 
+            continue
+
+        cursor.execute("DELETE FROM employees WHERE id = ?", (id,))
+        database.commit()
+        print("Employee removed successfully!")
+        break
+
+def record_attendance():
+    while True:
+        try:
+            print("To record an attendance, please add the following: ")
+            id = int(input("Enter the id of the employee you would like to record attendance: "))
+        except ValueError:
+            print("Uh-oh! Please enter a number!")
+            continue
+
+        cursor.execute("SELECT 1 from employees WHERE id = ? LIMIT 1", (id,))
+        exists = cursor.fetchone()
+
+        if not exists:
+            print("Uh-oh! There is no employee with that id!") 
+            continue
+        
+        while True:
+            date = input("Enter date of attendance (YYYY-MM-DD): ")
+            try:
+                hours = float(input("Enter the hours worked: "))
+                break
+            except ValueError:
+                print("Uh-oh! Please enter a number!")
+                continue
+        
+        cursor.execute("INSERT INTO attendance (employee_id, date, hours), VALUES (?, ?, ?)", (id, date, hours))
+        database.commit()
+        print("Attendance added successfully!")
+        break
+
+def update_attendance():
+    while True:
+        try:
+            print("To update an attendance, please add the following: ")
+            id = int(input("Enter the id of the employee you would like to record attendance: "))
+        except ValueError:
+            print("Uh-oh! Please enter a number!")
+            continue
+        
+        cursor.execute("SELECT 1 from attendance WHERE id = ? LIMIT 1", (id,))
+        exists = cursor.fetchone()
+
+        if not exists:
+            print("Uh-oh! There is no attendance with that id!") 
+            continue
+
 
 
 
@@ -77,6 +159,6 @@ def update_employee():
 
 def main():
     sql_database()
-
+    
 if __name__ == "__main__":
     main()
