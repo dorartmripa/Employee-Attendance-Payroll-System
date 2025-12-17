ff41ea4c991a29bd6558a55d6b50df9da0fa91f4
Salih Mripa <smripa@hotmail.com>
2025-12-17 21:03:24 +0000

    Updated Code

--- Employee Attendance & Payroll System.py
+++ Employee Attendance & Payroll System.py
@@ -129,7 +129,7 @@ def record_attendance():
                 print("Uh-oh! Please enter a number!")
                 continue
         
-        cursor.execute("INSERT INTO attendance (employee_id, date, hours), VALUES (?, ?, ?)", (id, date, hours))
+        cursor.execute("INSERT INTO attendance (employee_id, date, hours) VALUES (?, ?, ?)", (id, date, hours))
         database.commit()
         print("Attendance added successfully!")
         break
@@ -138,7 +138,7 @@ def update_attendance():
     while True:
         try:
             print("To update an attendance, please add the following: ")
-            id = int(input("Enter the id of the employee you would like to record attendance: "))
+            id = int(input("Enter the id of the attendance you would like to update: "))
         except ValueError:
             print("Uh-oh! Please enter a number!")
             continue
@@ -150,15 +150,138 @@ def update_attendance():
             print("Uh-oh! There is no attendance with that id!") 
             continue
 
+        while True:
+            choice = input("""What would you like to update:\n
+                        - Employee ID\n
+                        - Date\n
+                        - Hours\n
+                        Enter one of the options: """).strip().lower()
+            
+            if choice not in ["employee id", "date", "hours"]:
+                print("Uh-oh! Please enter a valid option!")
+                continue
 
+            while True:
+                if choice == "employee id":
+                    try:
+                        update = int(input("Enter the new employee id: "))
+                    except ValueError:
+                        print("Uh-oh! Please enter a number!")
+                        continue
+                elif choice == "hours":
+                    try:
+                        update = float(input("Enter the new hours: "))
+                    except ValueError:
+                        print("Uh-oh! Please enter a number!")
+                        continue
+                else:
+                    update = input("Enter the new date (YYYY-MM-DD): ")
+                break
+            break
 
+        cursor.execute(f"UPDATE attendance SET {choice} = ? WHERE id = ?", (update, id))
+        database.commit()
+        print("Attendance updated successfully!")
+        break
 
+def search():
+    while True:
+        try:
+            print("To search for an employee, please add the following: ")
+            id = int(input("Enter the id of the employee you want to search for: "))
+        except ValueError:
+            print("Uh-oh! Please enter a number!")
+            continue
+        
+        cursor.execute("SELECT * from employees WHERE id = ?", (id,))
+        exists = cursor.fetchall()
 
+        if not exists:
+            print("Uh-oh! There is no employee with that id!") 
+            continue
 
+        cursor.execute("SELECT * FROM employees")
+        header = [description[0] for description in cursor.description]
+        print(header)
+        print(exists, "\n")
 
+        cursor.execute("SELECT * from attendance WHERE employee_id = ?", (id,))
+        results = cursor.fetchall()
+
+        print("Employee's attendance record: ")
+        for result in results:
+            print(result)
+        
+        break
 
 def main():
+    choices = ["add employee", "update employee", "remove employee", "search employee", "record attendance", "update attendance", "view reports"]
     sql_database()
+
+    print("\n--- WELCOME TO EMPLOYEE ATTENDANCE & PAYROLL SYSTEM ---\n")
+
+    while True: #loops until user doesn't want to make any more changes to the database
+        print("Please select one of the following options: ")
+
+        print("\nEmployee Management:")
+        print("- Add employee")
+        print("- Update employee")
+        print("- Remove employee")
+        print("- Search employee")
+
+        print("\nAttendance Management:")
+        print("- Record attendance")
+        print("- Update attendance")
+        print("- View reports\n")
+
+        action = input("Enter your choice: ").strip().lower()
+
+        if action not in choices: #checks if user entered a valid option
+            while True:
+                print("\nOops! That's not a valid option. Try again.\n")
+                print("Please select one of the following options: ")
+
+                print("\nEmployee Management:")
+                print("- Add employee")
+                print("- Update employee")
+                print("- Remove employee")
+                print("- Search employee")
+
+                print("\nAttendance Management:")
+                print("- Record attendance")
+                print("- Update attendance")
+                print("- View reports\n")
+
+                action = input("Enter your choice: ").strip().lower()
+
+                if action in choices: #breaks out of the loop if a valid option was entered
+                    break
+        
+        if action == choices[0]: 
+            add_employee()
+        elif action == choices[1]:
+            update_employee()
+        elif action == choices[2]:
+            remove_employee()
+        elif action == choices[3]:
+            search()
+        elif action == choices[4]:
+            record_attendance()
+        elif action == choices[5]:
+            update_attendance()
+        else:
+            reports()
+
+        while True: #loops until user entered yer or no
+            yes_or_no = input("Do you want to perform another inventory action? (yes/no): ").strip().lower()
+
+            if yes_or_no in ["yes", "no"]:
+                break #Exits loop if user entered a valid option
+
+        if yes_or_no == "no": #ends code if user entered no
+            print("\nThank you for using Employee Attendance & Payroll System!\n")
+            database.close() #Closes connection to the database
+            break #Exits the loop to end the program
     
 if __name__ == "__main__":
     main()
